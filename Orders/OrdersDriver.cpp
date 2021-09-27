a7e80bb875da021fe2668af700975d4c2c082280
AbhayPanchoo <59373798+Abhay520@users.noreply.github.com>
2021-09-26 22:22:11 -0400

    Update OrdersDriver.cpp
    Need to redo the main method afterwards

--- Orders/OrdersDriver.cpp
+++ Orders/OrdersDriver.cpp
@@ -1,15 +1,1 @@
-#include "Orders.h"
 
-using namespace std;
-
-int main(){
-    OrdersList* ol = new OrdersList();
-    cout << "Inserting a few Orders into the Orders list" <<endl;
-    ol->insert(*new Advance());
-    ol->insert(*new Deploy());
-    ol->insert(*new Blockade());
-    ol->insert(*new Airlift());
-    ol->insert(*new Negotiate());
-    ol->insert(*new Bomb());
-    ol->print();
-}
