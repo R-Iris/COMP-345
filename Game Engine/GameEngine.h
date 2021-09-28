351f675e2f9c051f2efa10fab608fdb747d14b83
Ryan Amstutz <59939000+Xaelonous@users.noreply.github.com>
2021-09-28 01:42:27 -0400

    Completely refurbished the game engine
    now uses a vector that contains pointers to states
    
    destructor for transition (and possibly state) sucks rn

--- Game Engine/GameEngine.h
+++ Game Engine/GameEngine.h
@@ -1,39 +1,67 @@
-#pragma once         // Makes sure that the GameEngine.h file is only called once
-#include <string>    // Allows for the use of strings
-#include <iostream>  // Allows for the use of input and output streams
-using namespace std; // Imports the standard library for cout/cin..etc..
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-// Keeps track of the current state of the game, takes in input from command and checks if it's valid
+// Keeps track of the state's name as well as next available state
 class State
 {
 public:
-    // Default Constructor
-    State();
-    // Copy Constructor
-    State(State &state);
-    // Equals operator copy constructor
-    State &operator=(const State &state);
-    // Insertion operator conversion
-    friend ostream &operator<<(ostream &out, const State &state);
-    // Prints out the current state to the console
-    void showState();
-    // Retrieves the name of the current state
-    string getState();
-    // Checks validity of the command before executing
-    bool isValid(string command);
-    // Executes the command to change the state
-    bool changeState(string nextState);
+    // State's name
+    string stateName;
+    // Pointer to the next state
+    State *nextState;
 
-private:
-    // Array states that contains all the names for each state
-    string states[8];
-    // The current state
-    string currentState;
-    // Position in the array of the current state
-    int currentStatePosition;
+    // Default constructor
+    State();
+    // Destructor
+    ~State();
+    // Parametrized constructor
+    State(string name, State *nextState);
+    // Parametrized constructor
+    State(string name);
+    // Copy constructor
+    State(const State *state);
+    // = operator copy constructor
+    State &operator=(const State *state);
+    // << ostream conversion
+    friend ostream &operator<<(ostream &out, const State &state);
 };
 
-// Starts the game and sets up state machine
+// Transitions between states and contains the ruleset for the game engine
+class Transition
+{
+public:
+    // States
+    State *start;
+    State *map_loaded;
+    State *map_validated;
+    State *players_added;
+    State *assign_reinforcement;
+    State *issue_orders;
+    State *execute_orders;
+    State *win;
+    // Vector of pointers to states
+    vector<State *> states;
+    // Current State
+    State *currentState;
+    // Default Constructor
+    Transition();
+    // Destructor
+    ~Transition();
+    // Copy constructor
+    Transition(const Transition *transition);
+    // = operator copy constructor
+    Transition &operator=(const Transition *transition);
+    // << ostream conversion
+    friend ostream &operator<<(ostream &out, const Transition *transition);
+    // Checks and Changes the states
+    bool changeState(string command);
+};
+
+// Ends the game and deletes game object
+void end(Transition *game);
+
+// Starts and plays the game
 void playGame();
-// Ends the game without memory leaks
-void end(State *game);
