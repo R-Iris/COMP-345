ea2d236c12cd0a5e24d6def4ed9772b2f8340483
Kokipoki <karimgame1999@yahoo.com>
2021-09-28 13:31:33 -0400

    Structure

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -1,9 +1,1 @@
-#include <iostream>
-using namespace std;
 #include "Player.h" // Including the appropriate header file
-
-int main() {
-  cout << "This is a temporary file to initialize the folder.";
-  cout << "Push test.";
-  return 0;
-}
--- Player/Player.h
+++ Player/Player.h
@@ -1,5 +1,23 @@
 #pragma once // Should be included in all header files to avoid redundant inclusions. Dealt by preprocessor
+#include <iostream>
+using namespace std;
 
 class Player {
-  
+public:
+	void toDefend();	// return a list of territories that are to be defended
+	void toAttack();	// returns a list of territories that are to be attacked
+	void issueOrder();	// creates an Order object and puts it in the player’s list of orders
+	//Hand* hand;
+};
+
+class Territory {
+public:
+	string name;
+};
+
+class Hand {};
+
+class Order {
+public:
+	string order;
 };
