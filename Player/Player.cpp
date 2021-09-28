14a13bcfe5b2f7a092501b3941b56fe40f6e69d1
Kokipoki <karimgame1999@yahoo.com>
2021-09-28 16:27:59 -0400

    :)

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -1,1 +1,42 @@
 #include "Player.h" // Including the appropriate header file
+
+Player::Player() {
+	
+}
+
+Player::Player(string name, Hand* hand) : name(name), hand(hand)
+{
+}
+
+void Player::toDefend() // return a list of territories that are to be defended
+{
+
+}
+
+void Player::toAttack() // returns a list of territories that are to be attacked
+{
+
+}
+
+void Player::issueOrder(string order) // creates an Order object and puts it in the player’s list of orders
+{
+
+}
+
+string Player::getName() {return string();}
+Hand* Player::getHand() {return hand;}
+
+Hand::Hand() : name("Empty Hand") {}
+Hand::Hand(string name) : name(name) {}
+string Hand::getName() {return string();}
+void Hand::setName(string name) {name = name;}
+
+Territory::Territory() : name("Undiscovered") {}
+Territory::Territory(string name) : name(name) {}
+string Territory::getName() {return string();}
+void Territory::setName(string name) {name = name;}
+
+Order::Order() : order("Unknown") {}
+Order::Order(string order) : order(order) {}
+string Order::getOrder() {return string();}
+void Order::setOrder(string order) {order = order;}
--- Player/Player.h
+++ Player/Player.h
@@ -1,23 +1,51 @@
 #pragma once // Should be included in all header files to avoid redundant inclusions. Dealt by preprocessor
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Player {
 public:
+	Player();
+	Player(string name, class Hand*);
 	void toDefend();	// return a list of territories that are to be defended
 	void toAttack();	// returns a list of territories that are to be attacked
-	void issueOrder();	// creates an Order object and puts it in the player’s list of orders
-	//Hand* hand;
+	void issueOrder(string);	// creates an Order object and puts it in the player’s list of orders
+	string getName();
+	class Hand* getHand();
+
+private:
+	string name;
+	class Hand* hand;
+	vector<class Territory*> territories;
+	vector<class Order*> orders;
 };
 
 class Territory {
 public:
+	Territory();
+	Territory(string);
+	string getName();
+	void setName(string);
+private:
 	string name;
 };
 
-class Hand {};
+class Hand {
+public:
+	Hand();
+	Hand(string);
+	string getName();
+	void setName(string);
+private:
+	string name;
+};
 
 class Order {
 public:
+	Order();
+	Order(string);
+	string getOrder();
+	void setOrder(string);
+private:
 	string order;
 };
