06fbf1923e5b69b5c9e24da36828b4401c54d149
Ryan Amstutz <59939000+Xaelonous@users.noreply.github.com>
2021-09-30 19:27:57 -0400

    adapted gameengine to rubric

--- Game Engine/GameEngine.cpp
+++ Game Engine/GameEngine.cpp
@@ -2,183 +2,214 @@
 
 // Members of State Class
 
-State::State() : stateName("none"), nextState(nullptr)
+State::State() : stateName("none")
 {
 }
 
-State::State(string name, State *nextState) : stateName(name), nextState(nextState)
+State::State(string name) : stateName(name)
 {
 }
 
-State::State(string name) : stateName(name), nextState(nullptr)
+State::State(const State &state) : stateName(state.stateName)
 {
 }
 
-State::State(const State *state) : stateName(state->stateName), nextState(state->nextState)
+State &State::operator=(const State &state)
 {
-}
-
-State &State::operator=(const State *state)
-{
-    this->stateName = state->stateName;
-    this->nextState = state->nextState;
+    this->stateName = state.stateName;
     return *this;
 }
 
-ostream &operator<<(ostream &out, const State *state)
+ostream &operator<<(ostream &out, const State &state)
 {
-    out << "Current State: " << state->stateName << endl;
-    out << "Next Available State: " << state->nextState->stateName << endl;
+    out << "Current State: " << state.stateName << endl;
     return out;
 }
 
 // Members of Transition Class
 
-Transition::Transition()
+Transition::Transition() : current(nullptr), next(nullptr)
 {
-    // Initializing states
-    State *start = new State("start");
-    states.push_back(start);
-    State *map_loaded = new State("map_loaded");
-    states.push_back(map_loaded);
-    State *map_validated = new State("map_validated");
-    states.push_back(map_validated);
-    State *players_added = new State("players_added");
-    states.push_back(players_added);
-    State *assign_reinforcement = new State("assign_reinforcement");
-    states.push_back(assign_reinforcement);
-    State *issue_orders = new State("issue_orders");
-    states.push_back(issue_orders);
-    State *execute_orders = new State("execute_orders");
-    states.push_back(execute_orders);
-    State *win = new State("win");
-    states.push_back(win);
-    // Connecting states together
-    start->nextState = map_loaded;
-    map_loaded->nextState = map_validated;
-    map_validated->nextState = players_added;
-    players_added->nextState = assign_reinforcement;
-    assign_reinforcement->nextState = issue_orders;
-    issue_orders->nextState = execute_orders;
-    execute_orders->nextState = assign_reinforcement;
-    win->nextState = start;
-    // Initializing currentState
-    currentState = start;
-    // Announce current state
-    cout << currentState;
 }
 
 Transition::~Transition()
 {
-    states.clear();
-    currentState = nullptr;
+    if (!next) delete next;
+    delete current;
 }
 
-Transition::Transition(const Transition *transition) : states(transition->states), currentState(transition->currentState)
+Transition::Transition(State* current, State* next)  : current(current), next(next)
 {
 }
 
-Transition &Transition::operator=(const Transition *transition)
+Transition::Transition(const Transition &transition) : current(transition.current), next(transition.next)
 {
-    this->states = transition->states;
-    this->currentState = transition->currentState;
+}
+
+Transition &Transition::operator=(const Transition &transition)
+{
+    this->current = transition.current;
+    this->next = transition.next;
     return *this;
 }
 
-ostream &operator<<(ostream &out, const Transition *transition)
+ostream &operator<<(ostream &out, const Transition &transition)
 {
-    int size = transition->states.size();
-    for (int i = 0; i < size; i++)
+    out << "(From: " << transition.current->stateName << ", To: " << transition.current->stateName << ")" << endl;
+    return out;
+}
+
+GameEngine::GameEngine()
+{
+    // Initializing states
+    State* start = new State("start");
+    State* map_loaded = new State("map_loaded");
+    State* map_validated = new State("map_validated");
+    State* players_added = new State("players_added");
+    State* assign_reinforcement = new State("assign_reinforcement");
+    State* issue_orders = new State("issue_orders");
+    State* execute_orders = new State("execute_orders");
+    State* win = new State("win");
+    // Connecting states together
+    Transition* trans1 = new Transition(start, map_loaded);
+    Transition* trans2 = new Transition(map_loaded, map_validated);
+    Transition* trans3 = new Transition(map_validated, players_added);
+    Transition* trans4 = new Transition(players_added, assign_reinforcement);
+    Transition* trans5 = new Transition(assign_reinforcement, issue_orders);
+    Transition* trans6 = new Transition(issue_orders, execute_orders);
+    Transition* trans7 = new Transition(execute_orders, assign_reinforcement);
+    Transition* trans8 = new Transition(execute_orders, win);
+    Transition* trans9 = new Transition(win, start);
+    // Pushing transitions to transition vector
+    transitions.push_back(trans1);
+    transitions.push_back(trans2);
+    transitions.push_back(trans3);
+    transitions.push_back(trans4);
+    transitions.push_back(trans5);
+    transitions.push_back(trans6);
+    transitions.push_back(trans7);
+    transitions.push_back(trans8);
+    transitions.push_back(trans9);
+    // Initializing currentState
+    currentState = start;
+    // Announce current state
+    cout << *currentState;
+}
+
+GameEngine::~GameEngine()
+{
+    // Calls delete on every element inside of the vector
+    transitions.clear();
+    currentState = nullptr;
+}
+
+GameEngine::GameEngine(vector<Transition*>& transitions, State* currentState)
+{
+    this->transitions = transitions;
+    this->currentState = currentState;
+}
+
+GameEngine& GameEngine::operator=(const GameEngine& gameEngine) 
+{
+    this->transitions = gameEngine.transitions;
+    this->currentState = gameEngine.currentState;
+    return *this;
+}
+
+ostream& operator<<(ostream& out, const GameEngine& gameEngine)
+{
+    for (int i = 0; i < gameEngine.transitions.size(); i++)
     {
-        out << "State(" << i << "): " << transition->states.at(i)->stateName << endl;
+        out << *gameEngine.transitions.at(i) << endl;
     }
     return out;
 }
 
-bool Transition::changeState(string command)
+bool GameEngine::changeState(string command)
 {
-    if (currentState->stateName == states[0]->stateName && command == "loadmap")
+    if (currentState->stateName == transitions[0]->current->stateName && command == "loadmap")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[0]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[1] && command == "loadmap")
+    else if (currentState->stateName == transitions[1]->current->stateName && command == "loadmap")
     {
-        cout << currentState;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[1] && command == "validatemap")
+    else if (currentState->stateName == transitions[1]->current->stateName && command == "validatemap")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[1]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[2] && command == "addplayer")
+    else if (currentState->stateName == transitions[2]->current->stateName && command == "addplayer")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[2]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[3] && command == "addplayer")
+    else if (currentState->stateName == transitions[3]->current->stateName && command == "addplayer")
     {
-        cout << currentState;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[3] && command == "assigncountries")
+    else if (currentState->stateName == transitions[3]->current->stateName && command == "assigncountries")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[3]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[4] && command == "issueorder")
+    else if (currentState->stateName == transitions[4]->current->stateName && command == "issueorder")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[4]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[5] && command == "issueorder")
+    else if (currentState->stateName == transitions[5]->current->stateName && command == "issueorder")
     {
-        cout << currentState;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[5] && command == "endissueorders")
+    else if (currentState->stateName == transitions[5]->current->stateName && command == "endissueorders")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[5]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[6] && command == "execorder")
+    else if (currentState->stateName == transitions[6]->current->stateName && command == "execorder")
     {
-        cout << currentState;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[6] && command == "endexecorders")
+    else if (currentState->stateName == transitions[6]->current->stateName && command == "endexecorders")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[6]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[6] && command == "win")
+    else if (currentState->stateName == transitions[6]->current->stateName && command == "win")
     {
-        currentState = states[7];
-        cout << currentState;
+        currentState = transitions[7]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[7] && command == "play")
+    else if (currentState->stateName == transitions[8]->current->stateName && command == "play")
     {
-        currentState = currentState->nextState;
-        cout << currentState;
+        currentState = transitions[8]->next;
+        cout << *currentState;
         return true;
     }
-    else if (currentState == states[7] && command == "end")
+    else if (currentState->stateName == transitions[8]->current->stateName && command == "end")
     {
         return true;
     }
     return false;
 }
 
-void end(Transition *game)
+
+void end(GameEngine *game)
 {
     delete game; // Comment out to test copy constructor
     game = NULL; // Comment out to test copy constructor
@@ -189,9 +220,12 @@ void end(Transition *game)
 
 void playGame()
 {
-    Transition *game = new Transition();
-    // Transition *copygame = new Transition(*game); // Copied game using copy constructor
-    // cout << *game; // Test insertion operator
+    GameEngine *game = new GameEngine();
+    GameEngine *copygame = new GameEngine(*game); // Copied game using copy constructor
+    GameEngine* equalsGame = game;
+    cout << "Memory Address of Original game object: " << &game << endl;
+    cout << "Memory Address of Copied (=) game object: " << &equalsGame << endl;
+    cout << "GameEngine insertion operator: " << *game; // Test insertion operator
     string command = "";
     while (game->currentState->stateName != "win" || command != "end")
     {
@@ -202,21 +236,9 @@ void playGame()
             cout << "Command invalid, please try again: ";
             cin >> command;
         }
-        // cout << "copygame State: " << copygame->currentState << endl; // Test copied state object
+        cout << "copygame State: " << *copygame->currentState << endl; // Test copied state object
     }
+    delete copygame;
+    delete equalsGame;
     end(game);
-    // Test copy constructor
-    // while (copygame->getState() != "win" || command != "end")
-    // {
-    //     cout << "Please enter command: ";
-    //     cin >> command;
-    //     while (!copygame->isValid(command))
-    //     {
-    //         cout << "Command invalid, please try again: ";
-    //         cin >> command;
-    //     }
-    //     copygame->changeState(command);
-    //     cout << "\"game\" State: " << *game << endl;
-    // }
-    // end(copygame);
 }
