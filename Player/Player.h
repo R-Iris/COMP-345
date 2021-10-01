1b384b413924d1051bdef6e1cb0a03a16e216ea0
Kokipoki <karimgame1999@yahoo.com>
2021-09-30 20:29:02 -0400

    All done (except comments)

--- Player/Player.h
+++ Player/Player.h
@@ -5,18 +5,26 @@ using namespace std;
 
 class Player {
 public:
-	Player();
+	Player(class Hand*);
 	Player(string name, class Hand*);
-	void toDefend();	// return a list of territories that are to be defended
-	void toAttack();	// returns a list of territories that are to be attacked
+	Player(const Player &player);
+	~Player();
+	Player& operator =(const Player& player);
+	void addTerritoryDefend(class Territory*);
+	void addTerritoryAttack(class Territory*);
+	vector<class Territory*> toDefend();	// return a list of territories that are to be defended (owned territories)
+	vector<class Territory*> toAttack();	// returns a list of territories that are to be attacked
 	void issueOrder(string);	// creates an Order object and puts it in the player’s list of orders
 	string getName();
 	class Hand* getHand();
+	vector<class Order*> getOrders();
+	friend ostream& operator<<(ostream& out, const Player& player);
 
 private:
 	string name;
 	class Hand* hand;
-	vector<class Territory*> territories;
+	vector<class Territory*> territoriesDefend; // List of owned territories
+	vector<class Territory*> territoriesAttack; // List of territories to attack
 	vector<class Order*> orders;
 };
 
@@ -24,8 +32,11 @@ class Territory {
 public:
 	Territory();
 	Territory(string);
+	Territory(const Territory& territory);
+	Territory& operator =(const Territory& territory);
 	string getName();
 	void setName(string);
+	friend ostream& operator<<(ostream& out, const Territory& territory);
 private:
 	string name;
 };
@@ -34,8 +45,11 @@ class Hand {
 public:
 	Hand();
 	Hand(string);
+	Hand(const Hand& hand);
+	Hand& operator =(const Hand& hand);
 	string getName();
 	void setName(string);
+	friend ostream& operator<<(ostream& out, const Hand& hand);
 private:
 	string name;
 };
@@ -44,8 +58,11 @@ class Order {
 public:
 	Order();
 	Order(string);
+	Order(const Order& order);
+	Order& operator =(const Order& order);
 	string getOrder();
 	void setOrder(string);
+	friend ostream& operator<<(ostream& out, const Order& order);
 private:
-	string order;
+	string orderString;
 };
--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -1,11 +1,5 @@
 #include "Player.h" // Including the appropriate header file
 
-/*
-All classes must implement a correct copy constructor, assignment operator,and stream insertion operator.
-Memory leaks must be avoided.
-Code must be documented using comments
-*/
-
 int main() {
 	cout << "Driver for the player class." << endl;
 
@@ -13,7 +7,36 @@ int main() {
 	Territory *t1 = new Territory("T1"), *t2 = new Territory("T2"), *t3 = new Territory("T3"), *t4 = new Territory("T4"), *t5 = new Territory();
 	Order* o1 = new Order("O1"), * o2 = new Order("O2"), * o3 = new Order("O3"), * o4 = new Order("O4"), * o5 = new Order();
 
+	p1->addTerritoryDefend(t1); p1->addTerritoryDefend(t2);
+	p1->addTerritoryAttack(t3); p1->addTerritoryAttack(t4); p1->addTerritoryAttack(t5);
 	
+	cout << endl << "Territories to defend:" << endl;
+	for (Territory* t : p1->toDefend()) {
+		cout << *t << endl;
+	}
 
+	cout << endl << "Territories to attack:" << endl;
+	for (Territory* t : p1->toAttack()) {
+		cout << *t << endl;
+	}
+
+	p1->issueOrder("Issued Order");
+	p1->issueOrder("Second Issued Order");
+	cout << endl << "Player 1's orders:" << endl;
+	for (Order* o : p1->getOrders()) {
+		cout << *o << endl;
+	}
+
+	cout << endl << "Name of the hand of cards Player 1 stores: " << *(p1->getHand()) << endl;
+
+	cout << endl << "Player 1's owned territories:" << endl;
+	for (Territory* t : p1->toDefend()) {
+		cout << *t << endl;
+	}
+
+	delete p1; delete p2;
+	delete t1; delete t2; delete t3; delete t4; delete t5;
+	delete o1; delete o2; delete o3; delete o4; delete o5;	
+	
 	return 0;
 }
