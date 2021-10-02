fc018087fa7e0806d7c7c8bf3148b4c35b23f83e
Kokipoki <karimgame1999@yahoo.com>
2021-10-01 20:05:54 -0400

    Final version with comments

--- Player/Player.h
+++ Player/Player.h
@@ -1,44 +1,49 @@
 #pragma once // Should be included in all header files to avoid redundant inclusions. Dealt by preprocessor
-#include <iostream>
-#include <vector>
+#include <iostream> // Used for cout and stream insertion operator
+#include <vector> // Used for lists instead of arrays
 using namespace std;
+
+// The player class contains a Hand (of cards), a list of owned territories, and a list of issued orders
 
 class Player {
 public:
-	Player(class Hand*);
-	Player(string name, class Hand*);
-	Player(const Player &player);
-	~Player();
-	Player& operator =(const Player& player);
-	void addTerritoryDefend(class Territory*);
-	void addTerritoryAttack(class Territory*);
-	vector<class Territory*> toDefend();	// return a list of territories that are to be defended (owned territories)
-	vector<class Territory*> toAttack();	// returns a list of territories that are to be attacked
-	void issueOrder(string);	// creates an Order object and puts it in the player’s list of orders
-	string getName();
-	class Hand* getHand();
-	vector<class Order*> getOrders();
-	friend ostream& operator<<(ostream& out, const Player& player);
+	Player(class Hand*); // Default constructor requires at least a hand
+	Player(string name, class Hand*); // Constructor with player name and hand
+	Player(const Player &player); // Copy constructor
+	~Player(); // Destructor
+	Player& operator =(const Player& player); // Assignment operator
+	void addTerritoryDefend(class Territory*); // Add territory to owned territories vector
+	void addTerritoryAttack(class Territory*); // PLACEHOLDER : add territory to vector of territories to attack
+	vector<class Territory*> toDefend();	// Returns a list of territories that are to be defended (owned territories)
+	vector<class Territory*> toAttack();	// Returns a list of territories that are to be attacked
+	void issueOrder(string);	// Creates an Order object and puts it in the player’s list of orders
+	string getName(); // Name getter
+	class Hand* getHand(); // Hand pointer getter
+	vector<class Order*> getOrders(); // PLACEHOLDER : Returns vector of orders
+	friend ostream& operator<<(ostream& out, const Player& player); // Stream insertion operator
 
 private:
 	string name;
-	class Hand* hand;
+	class Hand* hand; // Pointer to hand of cards
 	vector<class Territory*> territoriesDefend; // List of owned territories
 	vector<class Territory*> territoriesAttack; // List of territories to attack
-	vector<class Order*> orders;
+	vector<class Order*> orders; // List of orders issued
 };
+
+// Territory, Hand, and Order classes are placeholders and all similar in functionality
+// Comments made in Territory are similar for Hand and Order
 
 class Territory {
 public:
-	Territory();
-	Territory(string);
-	Territory(const Territory& territory);
-	Territory& operator =(const Territory& territory);
-	string getName();
-	void setName(string);
-	friend ostream& operator<<(ostream& out, const Territory& territory);
+	Territory(); // Default constructor
+	Territory(string); // Constructor to initialize string
+	Territory(const Territory& territory); // Copy constructor
+	Territory& operator =(const Territory& territory); // Assignment operator
+	string getName(); // String getter
+	void setName(string); // String setter 
+	friend ostream& operator<<(ostream& out, const Territory& territory); // Stream insertion operator
 private:
-	string name;
+	string name; // Placeholder string
 };
 
 class Hand {
--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -3,23 +3,29 @@
 int main() {
 	cout << "Driver for the player class." << endl;
 
+	// Create new player, territory, order, and hand objects
+	// Using pointers as they are user-created classes
 	Player *p1 = new Player("P1", new Hand()), * p2 = new Player("P2", new Hand());
 	Territory *t1 = new Territory("T1"), *t2 = new Territory("T2"), *t3 = new Territory("T3"), *t4 = new Territory("T4"), *t5 = new Territory();
 	Order* o1 = new Order("O1"), * o2 = new Order("O2"), * o3 = new Order("O3"), * o4 = new Order("O4"), * o5 = new Order();
 
+	// Add territories to defend and attack for Player 1
 	p1->addTerritoryDefend(t1); p1->addTerritoryDefend(t2);
 	p1->addTerritoryAttack(t3); p1->addTerritoryAttack(t4); p1->addTerritoryAttack(t5);
 	
+	// Printing out the territories to defend for Player 1
 	cout << endl << "Territories to defend:" << endl;
 	for (Territory* t : p1->toDefend()) {
 		cout << *t << endl;
 	}
 
+	// Printing out the territories to attack for Player 1
 	cout << endl << "Territories to attack:" << endl;
 	for (Territory* t : p1->toAttack()) {
 		cout << *t << endl;
 	}
 
+	// Issuing and printing out orders given to Player 1
 	p1->issueOrder("Issued Order");
 	p1->issueOrder("Second Issued Order");
 	cout << endl << "Player 1's orders:" << endl;
@@ -27,16 +33,24 @@ int main() {
 		cout << *o << endl;
 	}
 
+	// Printing the name of player 1's hand
 	cout << endl << "Name of the hand of cards Player 1 stores: " << *(p1->getHand()) << endl;
 
+	// Printing the list of player 1's owned territories
 	cout << endl << "Player 1's owned territories:" << endl;
 	for (Territory* t : p1->toDefend()) {
 		cout << *t << endl;
 	}
-
+	
+	// Delete all pointers that a "new" assignment on the right-hand side
 	delete p1; delete p2;
 	delete t1; delete t2; delete t3; delete t4; delete t5;
 	delete o1; delete o2; delete o3; delete o4; delete o5;	
 	
+	// Avoid dangling pointers by assigning NULL to deleted pointers
+	p1 = NULL; p2 = NULL;
+	t1 = NULL; t2 = NULL; t3 = NULL; t4 = NULL; t5 = NULL;
+	o1 = NULL; o2 = NULL; o3 = NULL; o4 = NULL; o5 = NULL;
+
 	return 0;
 }
