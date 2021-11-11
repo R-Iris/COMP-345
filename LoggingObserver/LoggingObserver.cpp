96dce47513fbee5c18fc64d1964b2116fdf6e67c
Ryan Amstutz <59939000+Xaelonous@users.noreply.github.com>
2021-11-11 18:16:37 -0500

    unused constructors/ stream insertion operators for rubric
    /shrug

--- LoggingObserver/LoggingObserver.cpp
+++ LoggingObserver/LoggingObserver.cpp
@@ -1,5 +1,17 @@
 #include "../LoggingObserver/LoggingObserver.h"
 
+ILoggable::ILoggable() {}
+ILoggable::ILoggable(const ILoggable& s) {}
+ILoggable& ILoggable::operator=(const ILoggable& s) { return *this; }
+
+Subject::Subject() {}
+Subject::~Subject() { this->_observer = nullptr; }
+Subject::Subject(const Subject& s) : _observer(s._observer) {}
+Subject& Subject::operator=(const Subject& s) 
+{ 
+	this->_observer = s._observer; 
+	return *this;
+}
 
 void Subject::Attach(Observer* _logger) {
 	_observer = _logger;
@@ -13,6 +25,14 @@ void Subject::Notify(ILoggable* _loggable_object) {
 	if (_observer != nullptr) _observer->Update(_loggable_object);
 }
 
+Observer::Observer() {}
+Observer::Observer(const Observer& s) {}
+Observer& Observer::operator=(const Observer& s) { return *this; }
+
+LogObserver::LogObserver() {}
+LogObserver::LogObserver(const LogObserver& s) {}
+LogObserver& LogObserver::operator=(const LogObserver& s) { return *this; }
+
 void LogObserver::Update(ILoggable* _loggable_object)
 {
 	// opening output file stream
@@ -21,3 +41,8 @@ void LogObserver::Update(ILoggable* _loggable_object)
 	logfile << _loggable_object->stringToLog() << endl; // output the stringToLog to the output file
 	logfile.close(); // close the output file stream
 }
+
+ostream& operator<<(ostream& os, const ILoggable& s) { return os; }
+ostream& operator<<(ostream& os, const Observer& s) { return os; }
+ostream& operator<<(ostream& os, const LogObserver& s) { return os; }
+ostream& operator<<(ostream& os, const Subject& s) { return os; }
